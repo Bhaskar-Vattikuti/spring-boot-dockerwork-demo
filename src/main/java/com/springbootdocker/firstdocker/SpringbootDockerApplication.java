dde7387bd669ff327dbb958d0940eec9ddba50ba
RAVILLA TEJA KUMAR <ravilla_kumar@persistent.com>
2022-12-16 22:25:26 +0530

    springboot files added

---
+++ src/main/java/com/springbootdocker/firstdocker/SpringbootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.springbootdocker.firstdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+	@GetMapping("/getmsg")
+	public String getmsg()
+	{
+		return "hai this is tejas";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdocker/firstdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdocker.firstdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
